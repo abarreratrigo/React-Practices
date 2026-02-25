f83e59faa6c25bd25201e80a0ebfadd6cc748e3b
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-02-25 12:00:42 +0100

    [UPDATE] First gifs added

--- gif-app/src/GifsApp.tsx
+++ gif-app/src/GifsApp.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { mockGifs } from './mock-data/gifs.mock'
 
 export const GifsApp = () => {
     return (
@@ -27,7 +28,17 @@ export const GifsApp = () => {
 
             {/*Gifs*/}
             <div className="gifs-container">
-                
+                {
+                    mockGifs.map((gif) => (
+                        <div key={gif.id} className='gif-card'>
+                            <img src={gif.url} alt={gif.title} />
+                            <h3>{gif.title}</h3>
+                            <p>
+                                {gif.width}x{gif.height} (1.5mb)
+                            </p>
+                       </div> 
+                    ))
+                }
             </div>
         </>
   )
