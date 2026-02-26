e9a1f6aae122d141528b232806e1022bee8df49b
Alexis Barrera <alexis.barrera@rotherindustrial.es>
2026-02-26 08:53:01 +0100

    [ADD] Custom Hook created (only a test)

---
+++ gif-app/src/counter/hooks/useCounter.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const useCounter = (initialValue: number = 0) => {
+
+    const [counter, setCounter] = useState(initialValue)
+
+    const handleAdd = () => {
+        setCounter(counter +1)
+    }
+
+    //Doing this in case that we can't access to "counter"
+    const handleSubstract = () => {
+        setCounter ((prevState) => prevState-1)
+    }
+
+    const handleReset = () => {
+        setCounter (initialValue)
+    }
+    
+    return {
+        //Properties
+        counter,
+
+        //Methods/actions
+        handleAdd,
+        handleSubstract, 
+        handleReset,
+    }
+}
